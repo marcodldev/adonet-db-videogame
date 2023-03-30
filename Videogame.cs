e1e5ae9772383cf8639b2873219b30372d679136
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-03-30 16:06:01 +0200

    + classi

---
+++ Videogame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet_db_videogame
+{
+    public class Videogame
+    {
+
+        public Videogame(string name, string overview, DateTime releaseDate, DateTime createdAt, DateTime updatedAt, int softwareHouseId)
+        {
+            Id = this.Id;
+            Name = name;
+            Overview = overview;
+            ReleaseDate = releaseDate;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            SoftwareHouseId = softwareHouseId;
+        }
+
+             int Id { get; }
+             string Name { get; set; }
+             string Overview { get; set; }
+             DateTime ReleaseDate { get; set; }
+             DateTime CreatedAt { get; set; }
+             DateTime UpdatedAt { get; set; }
+             int SoftwareHouseId { get; set; }
+        
+
+    }
+}
---
+++ VideogameManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace adonet_db_videogame
+{
+  
+        public class VideogameManager
+        {
+            private readonly SqlConnection connection;
+
+
+            public VideogameManager(SqlConnection connection)
+            {
+                this.connection = connection;
+            }
+
+            public void InserisciVideogame(Videogame videogame)
+            {
+          
+            }
+
+            public void RicercaVideogamePerId(int id)
+            {
+               
+            }
+
+            public void RicercaVideogiochiPerNome()
+            {
+
+            }
+
+            public void CancellaVideogame()
+            {
+
+            }
+    }
+    
+
+}
