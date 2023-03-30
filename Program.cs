9be13684fdeef4bd5583ac9b3d521bf4cd6d87c4
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-03-30 15:24:47 +0200

    + test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data.SqlClient;
+
+    string connectionString = "Data Source=localhost;Initial Catalog=videogames_db;Integrated Security=True";
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        try
+        {
+           // connection.Open();   using prima dell'istanza della connessione sostituisce open e close in try e finally
+
+            Console.WriteLine("Connessione al database riuscita");
+
+            var manager = new VideogameManager(connection);
+
+            while (true)
+            {
+                Console.WriteLine("Seleziona un'opzione:");
+                Console.WriteLine("1 - Inserisci un nuovo videogame");
+                Console.WriteLine("2 - Ricerca un videogame per ID");
+                Console.WriteLine("3 - Ricerca videogiochi per nome");
+                Console.WriteLine("4 - Cancella un videogame");
+                Console.WriteLine("5 - Esci");
+
+                var scelta = Convert.ToInt32(Console.ReadLine());
+
+                switch (scelta)
+                {
+                    case 1:
+                        manager.InserisciVideogame(CreaVideogameDaInput());
+                        break;
+
+                    case 2:
+                        RicercaVideogamePerId(manager);
+                        break;
+
+                    case 3:
+                        RicercaVideogiochiPerNome(manager);
+                        break;
+
+                    case 4:
+                        manager.CancellaVideogame(int id);
+                        break;
+
+                    case 5:
+                        return;
+
+                    default:
+                        Console.WriteLine("Scelta non valida");
+                        break;
+                }
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine($"Errore di connessione al database: \n {ex}");
+
+        }
+    }
